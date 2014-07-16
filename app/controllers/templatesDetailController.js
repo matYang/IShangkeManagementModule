f6d09da7c1a18df7c7df103e1fa6f1a2bf8afc9a
jzlingmo <120237546@qq.com>
2014-07-16 18:08:34 +0800

    create course detail  template controller

---
+++ app/controllers/templatesDetailController.js
@@ -0,0 +1,19 @@
+'use strict';
+appControllers.controller('templatesDetailCtrl',
+    ['$scope','restAPI','$state', function ($scope,restAPI,$state) {
+        var Templates = restAPI.templates;
+        var id = $state.params.id;
+        $scope.title = '模板详细信息';
+
+        //刷新列表
+        $scope.doRefresh = function(){
+
+            Templates.get({ID:id},function(data){
+                $scope.item = data.data;
+            },function(){
+                //error
+            });
+        };
+        $scope.doRefresh();
+    }]
+);
