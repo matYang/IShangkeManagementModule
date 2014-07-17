c1dda906132e841b1826fdd7fd8731d7ac8716f3
jzlingmo <120237546@qq.com>
2014-07-17 20:25:19 +0800

    templates:add query

--- app/controllers/templatesCreateController.js
+++ app/controllers/templatesCreateController.js
@@ -6,9 +6,9 @@ appControllers.controller('templatesCreateCtrl',
 
         //提交新建的模板
         $scope.submit_template = function(template){
-            Templates.save(template,function(){
+            Templates.save(template,function(data){
                 //todo create success to do something
-                app.toaster.pop('success', "创建成功", "");
+                app.toaster.pop('success', "课程模板创建成功", "");
                 app.log.info('create template success');
                 app.state.go('admin.templates');
 
