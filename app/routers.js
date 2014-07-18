a2633e5e4775d6bc7f6655bdf6f9affcc2ddda52
jzlingmo <120237546@qq.com>
2014-07-18 13:27:42 +0800

    change router config add abstract routers

--- app/routers.js
+++ app/routers.js
@@ -1,44 +1,52 @@
 var appRoutes = angular.module('appRoutes', []);
 
 appRoutes.config(function ($stateProvider, $urlRouterProvider, $httpProvider, $locationProvider) {
-    $urlRouterProvider.otherwise("/admin");
+    $urlRouterProvider.otherwise("/login");
     $stateProvider
-        //登录
+        /**************** 登录页面 ****************/
         .state('login', {
             url: '/login',
             templateUrl: 'views/login.html',
             controller: 'loginCtrl'
         })
-        //abstract state
+
+        /* admin abstract page 用于显示通用的组件 导航条*/
         .state('admin', {
             abstract: true,
             url: '/admin',
-            templateUrl: 'views/admin.html',
-            controller: 'homeCtrl'
+            templateUrl: 'views/admin.html'
         })
-        //!admin登录主页面
+
+        /**************** admin登录主页面 ****************/
         .state('admin.home', {
             url: '',
             templateUrl: 'views/admin/home.html',
             controller: 'homeCtrl'
         })
-        //!新单处理页面
+
+        /**************** 新单处理页面 ****************/
         .state('admin.new_orders', {
             url: '/new_orders',
             templateUrl: 'views/admin/new_orders.html',
             controller: 'newOrdersCtrl'
         })
-        //!旧单审核页面
+
+        /**************** 旧单审核页面 ****************/
         .state('admin.old_orders', {
             url: '/old_orders',
             templateUrl: 'views/admin/old_orders.html',
             controller: 'oldOrdersCtrl'
         })
 
-        /* 课程信息管理 */
-        //课程管理页面
+        /*************** 课程信息管理 ***************/
         .state('admin.courses', {
+            abstract: true,
             url: '/courses',
+            template: '<div ui-view></div>'
+        })
+        //课程管理页面
+        .state('admin.courses.list', {
+            url: '',
             templateUrl: 'views/admin/courses.html',
             controller: 'coursesCtrl'
         })
@@ -55,10 +63,17 @@ appRoutes.config(function ($stateProvider, $urlRouterProvider, $httpProvider, $l
             controller: 'coursesDetailCtrl'
         })
 
-        /* 课程模板管理 */
-        //课程模板列表
+
+
+        /**************** 课程模板管理 ****************/
         .state('admin.templates', {
+            abstract: true,
             url: '/templates',
+            template: '<div ui-view></div>'
+        })
+        //课程模板列表
+        .state('admin.templates.list', {
+            url: '',
             templateUrl: 'views/admin/templates.html',
             controller: 'templatesCtrl'
         })
