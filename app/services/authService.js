4f0a5bc011e0ae66a72e41a5926da00c9088c065
jzlingmo <120237546@qq.com>
2014-07-22 15:53:13 +0800

    add auth api

--- app/services/authService.js
+++ app/services/authService.js
@@ -1,42 +1,49 @@
 'use strict';
 appServices.factory('Auth',
-    ['$rootScope', '$cookieStore', '$http', '$q', '$location',
-        function ($rootScope, $cookieStore, $http, $q, $location) {
+    ['$rootScope', '$cookieStore', 'restAPI', '$q', '$location',
+        function ($rootScope, $cookieStore, restAPI, $q, $location) {
+            var auth = restAPI.auth;
             return {
+                //该方法主要用于同步内存和cookie中的状态
                 checkUser: function () {
-                    //检测用户的状态（从内存中的用户信息以及cookie中的信息）
+                    //检测用户的状态（从内存中的用户信息以及cookie中的信息） user的初始值为null
                     if (!$rootScope.global.user) {
+                        //todo 这里的cookie的key name也需要后续进行修改
                         $rootScope.global.user = $cookieStore.get('user');
                     }
-                    //内存中的用户登录状态 这里根据用户信息是否存在判断是否登录，也可根据
+                    //内存中的用户登录状态 这里根据基于上一步的用户信息是否存在判断是否登录
                     $rootScope.global.isLogin = !!$rootScope.global.user;
-                    //future 这里根据用户的role设置登录者的权限 暂时只做admin
-//                    $rootScope.global.isAdmin = !!($rootScope.user && $rootScope.global.user.role === 7);
+                    //这里根据用户的role重新设置登录者的身份 暂时只做admin
+                    $rootScope.global.isAdmin = !!($rootScope.user && $rootScope.global.user.group.indexOf('admin') == 0);
                     console.log('check user:' + JSON.stringify($rootScope.global));
                 },
-                login: function (data) {
-                    //这里使用promise模式 在controller中调用login先进行一下处理流程
+                login: function (data) {//date为登录信息对象 包含username和password
+                    //这里使用promise模式 在controller中调用login先进行以下处理流程
                     var defer = $q.defer();
                     var self = this;
-                    //todo 需要确定用户登录的api 以及fake api 并在assert中的data文件夹中加入fake json数据用于返回
-                    $http.post('api/login', data).success(function (result) {
-                        //todo 根据返回的用户信息设置内存中保存的用户信息
-                        $rootScope.global.user = data;//for test 这里应使用result中返回的用户信息
-                        //todo 这里还需要设置cookie
-                        $cookieStore.put('access_token',result.access_token);
-                        $cookieStore.put('user',result.user);
+                    var role = $rootScope.global.isAdmin ? 'admin' : 'partner';
+                    auth.post(angular.extend(data, {RO: role}), function (result) {
+                        //根据返回的用户信息设置内存中保存的用户信息 以及cookie
+                        $rootScope.global.user = result.user;//for test 这里应使用result中返回的用户信息
+                        //todo 由于cookie是在后台写入 这里后面确定了key name后再进行修改
+                        $cookieStore.put('access_token', result.access_token);
+                        $cookieStore.put('user', result.user);
                         self.checkUser();
                         defer.resolve('login success');
-                    }).error(function () {
-                            defer.reject('login failed');
-                        });
-
+                    }, function () {
+                        defer.reject('login failed');
+                    });
                     return defer.promise;
                 },
                 logout: function () {
                     var self = this;
-                    //todo 这里需要发送用户退出请求
-
+                    var role = $rootScope.global.isAdmin ? 'admin' : 'partner';
+                    //发送用户注销请求
+                    auth.delete({RO: role}, function () {
+                        //success
+                    }, function () {
+                        //error
+                    });
                     //清除cookie中存储的信息 以及内存中的用户信息 并返回到登录页面
                     $cookieStore.remove('access_token');
                     $cookieStore.remove('user');
--- app/services/restAPI.js
+++ app/services/restAPI.js
@@ -12,7 +12,9 @@ appServices.factory('restAPI', ['$resource', 'app',
         var api_config = {
             resources: {
                 //[0] is the fake api,[1] is the real api
-                'user': ['/data/user.json', '/user/:ID/:OP'],
+                //RO--role ID--id OP--operate
+                // Example request api: /api/v2/user/login /api/v2/admin/login
+                'auth': ['/data/:RO.json/:OP', '/:RO/:OP'],
                 'templates': ['/data/templates:ID.json/:OP', '/template/:ID/:OP'],
                 'courses': ['/data/courses:ID.json', '/course/:ID/:OP'],
                 'bookings': ['/data/bookings:ID.json', '/booking/:ID/:OP']
@@ -22,7 +24,7 @@ appServices.factory('restAPI', ['$resource', 'app',
             var prefix = '/api/' + app.version;
             var url = app.test_mode ? api_config.resources[recourseName][0] : prefix + api_config.resources[recourseName][1];
             //ID is the resource id and OP is operation name like 'submit' 'cancel'
-            return $resource(url, {ID:'@ID',OP:'@OP'},
+            return $resource(url, {ID:'@ID',OP:'@OP',RO:'@RO'},//api中前缀为:对应的变量会从数据中的ID/OP/RO中匹配
                 {
                     'update': { method: 'PUT' },
                     'operate': { method: 'POST' }
@@ -30,9 +32,11 @@ appServices.factory('restAPI', ['$resource', 'app',
         };
 
         return {
-            'user': resource_maker('user'),
+            'auth': resource_maker('auth'),
             'templates': resource_maker('templates'),
-            'courses': resource_maker('courses')
+            'courses': resource_maker('courses'),
+            'bookings': resource_maker('bookings')
+
         };
     }
 ]);
