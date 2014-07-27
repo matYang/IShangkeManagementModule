3a99e2e03e6cefa2aff1bb07991580f23a49ab07
FinalSpark <godomoe@gmail.com>
2014-07-27 21:47:54 +0800

    photo upload
    looks right to me

--- app/controllers/ctrl.partners.photo.js
+++ app/controllers/ctrl.partners.photo.js
@@ -1,82 +1,53 @@
 'use strict';
-appControllers.controller('partnersEditPhotoCtrl',
-    ['$scope','restAPI','$state', '$upload', function ($scope,restAPI,$state) {
+appControllers.controller('partnersPhotoCtrl',
+    ['$scope','restAPI','$state', '$upload', 'app', function ($scope, restAPI, $state, $upload, app) {
         var Partners = restAPI.partners;
         var id = $state.params.id;
         var uploadUrl = "/tempurl"
         $scope.imgs = [{}];
-          $scope.usingFlash = FileAPI && FileAPI.upload != null;
-        $scope.fileReaderSupported = window.FileReader != null && (window.FileAPI == null || FileAPI.html5 != false);
-        $scope.uploadRightAway = false;
-        $scope.hasUploader = function(index) {
-            return $scope.upload[index] != null;
-        };
-        $scope.onFileSelect = function($files) {
-            $scope.selectedFiles = [];
-            $scope.progress = [];
-            if ($scope.upload && $scope.upload.length > 0) {
-                for (var i = 0; i < $scope.upload.length; i++) {
-                    if ($scope.upload[i] != null) {
-                        $scope.upload[i].abort();
-                    }
-                }
-            }
-            $scope.upload = [];
-            $scope.uploadResult = [];
-            $scope.selectedFiles = $files;
-            $scope.dataUrls = [];
+        $scope.onFileSelect = function($files, $index) {
             for ( var i = 0; i < $files.length; i++) {
                 var $file = $files[i];
+                $scope.imgs[$index].photo = $file;
                 if ($scope.fileReaderSupported && $file.type.indexOf('image') > -1) {
                     var fileReader = new FileReader();
                     fileReader.readAsDataURL($files[i]);
                     var loadFile = function(fileReader, index) {
                         fileReader.onload = function(e) {
-                            $timeout(function() {
-                                $scope.dataUrls[index] = e.target.result;
-                            });
                         }
                     }(fileReader, i);
                 }
-                $scope.progress[i] = -1;
-                if ($scope.uploadRightAway) {
-                    $scope.start(i);
-                }
             }
         };
-        $scope.startAll = function() {
-            for (var i = 0; i < $scope.selectedFiles.length; i++) {
-              $scope.start(i);
+        $scope.upload = function() {
+            var files = [], fileNames = [];
+            for (var i = 0; i < $scope.imgs.length; i++) {
+                files[i] = $scope.imgs[i].photo;
+                fileNames[i] = "classImg" + (i + 1);
+                $scope.imgs[i].photo = null;
             }
-        };
-        $scope.start = function(index) {
-            $scope.progress[index] = 0;
             $scope.errorMsg = null;
 
-            $scope.upload[index] = $upload.upload({
+            $upload.upload({
                 url: uploadUrl,
                 method: "POST",
-                headers: {'my-header': 'my-header-value'},
-                data : {
-                    myModel : $scope.myModel
-                },
-                file: $scope.selectedFiles[index],
-                fileFormDataName: 'classImg' + (index + 1)
-            });
-            $scope.upload[index].then(function(response) {
-                app.toaster.pop("success", "照片上传成功", "");
-                $timeout(function() {
-                    $scope.uploadResult.push(response.data);
-                });
-            }, function(response) {
-                if (response.status > 0) $scope.errorMsg = response.status + ': ' + response.data;
-            }, function(evt) {
+                data : {},
+                file: files,
+                fileFormDataName: fileNames
+            }).success(function(response) {
+                if (response.status > 0) {
+                    app.toaster.pop("error", "照片添加失败", "");
+                    $scope.errorMsg = response.status + ': ' + response.data;
+                } else {
+                    app.toaster.pop("success", "照片添加成功", "");
+                    app.state.go("admin.partners.detail", {id: id});
+                }
+            }).progress(function(evt) {
                 // Math.min is to fix IE which reports 200% sometimes
                 $scope.progress[index] = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
             });
-            $scope.upload[index].xhr(function(xhr){
-            });
         };
+
         $scope.addPhoto = function () {
           $scope.imgs[$scope.imgs.length] = {};
         }
--- app/controllers/ctrl.partners.teacher.js
+++ app/controllers/ctrl.partners.teacher.js
@@ -1,79 +1,56 @@
 'use strict';
 appControllers.controller('partnersTeacherCtrl',
-    ['$scope','restAPI','$state', '$upload', function ($scope, restAPI, $state, $upload) {
+    ['$scope','restAPI','$state', '$upload', 'app', function ($scope, restAPI, $state, $upload, app) {
         var Partners = restAPI.partners;
         var id = $state.params.id;
         var uploadUrl = "/tempurl"
         $scope.teachers = [{}];
         $scope.fileReaderSupported = window.FileReader != null && (window.FileAPI == null || FileAPI.html5 != false);
-        $scope.uploadRightAway = false;
         $scope.hasUploader = function(index) {
             return $scope.upload[index] != null;
         };
-        $scope.onFileSelect = function($files) {
-            $scope.selectedFiles = [];
-            $scope.progress = [];
-            if ($scope.upload && $scope.upload.length > 0) {
-                for (var i = 0; i < $scope.upload.length; i++) {
-                    if ($scope.upload[i] != null) {
-                        $scope.upload[i].abort();
-                    }
-                }
-            }
-            $scope.upload = [];
-            $scope.uploadResult = [];
-            $scope.selectedFiles = $files;
-            $scope.dataUrls = [];
+        $scope.onFileSelect = function($files, $index) {
             for ( var i = 0; i < $files.length; i++) {
                 var $file = $files[i];
+                $scope.teachers[$index].teacherImg = $file;
                 if ($scope.fileReaderSupported && $file.type.indexOf('image') > -1) {
                     var fileReader = new FileReader();
                     fileReader.readAsDataURL($files[i]);
                     var loadFile = function(fileReader, index) {
                         fileReader.onload = function(e) {
-                            $timeout(function() {
-                                $scope.dataUrls[index] = e.target.result;
-                            });
                         }
                     }(fileReader, i);
                 }
-                $scope.progress[i] = -1;
-                if ($scope.uploadRightAway) {
-                    $scope.start(i);
-                }
             }
         };
-        $scope.startAll = function() {
-            for (var i = 0; i < $scope.selectedFiles.length; i++) {
-              $scope.start(i);
+        $scope.upload = function() {
+            var files = [], fileNames = [];
+            for (var i = 0; i < $scope.teachers.length; i++) {
+                files[i] = $scope.teachers[i].teacherImg;
+                fileNames[i] = "teacherImg" + (i + 1);
+                $scope.teachers[i].teacherImg = null;
             }
-        };
-        $scope.start = function(index) {
-            $scope.progress[index] = 0;
             $scope.errorMsg = null;
 
-            $scope.upload[index] = $upload.upload({
+            $upload.upload({
                 url: uploadUrl,
                 method: "POST",
                 data : {
                     teacherList : $scope.teachers
                 },
-                file: $scope.selectedFiles[index],
-                fileFormDataName: 'classImg' + (index + 1)
-            });
-            $scope.upload[index].then(function(response) {
-                $timeout(function() {
-
-                    $scope.uploadResult.push(response.data);
-                });
-            }, function(response) {
-                if (response.status > 0) $scope.errorMsg = response.status + ': ' + response.data;
-            }, function(evt) {
+                file: files,
+                fileFormDataName: fileNames
+            }).success(function(response) {
+                if (response.status > 0) {
+                    app.toaster.pop("error", "教师添加失败", "");
+                    $scope.errorMsg = response.status + ': ' + response.data;
+                } else {
+                    app.toaster.pop("success", "教师添加成功", "");
+                    app.state.go("admin.partners.detail", {id: id});
+                }
+            }).progress(function(evt) {
                 // Math.min is to fix IE which reports 200% sometimes
                 $scope.progress[index] = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
-            });
-            $scope.upload[index].xhr(function(xhr){
-//              xhr.upload.addEventListener('abort', function() {console.log('abort complete')}, false);
             });
         };
         $scope.addTeacher = function () {
