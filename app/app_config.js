27f9e892bca1950aedba7cc8b2546bb62d6ed6ed
jzlingmo <120237546@qq.com>
2014-07-28 16:29:30 +0800

    image filter

--- app/app_config.js
+++ app/app_config.js
@@ -3,6 +3,9 @@ app.constant('app', { //constant 'app' assemble things like restAPI and configs
     version: 'v2', //api版本信息
     test_mode: true, //测试模式
 
+    small_img : "",//小图的url前缀
+    large_img : "",//大图的url前缀
+
     //默认的分页
     default_page: {
         index: 1,    // 起始页
--- app/filters/filters.js
+++ app/filters/filters.js
@@ -2,7 +2,28 @@
 
 /* Filters */
 appFilters
+    /**
+     * 图片地址的filter
+     * @input array [imageName,partner_id]
+     * @return string <absolute image url>
+     */
+    .filter('smallImg', ['app',
+        function (app) {
+            return function (value) {
+                return app.small_img + value[1]+'/'+value[0];
+            };
+        }
+    ])
+    .filter('largeImg', ['app',
+        function (app) {
+            return function (value) {
+                return app.large_img + value[1]+'/'+value[0];
+            };
+        }
+    ])
+
     //以下为需要作为option并且value to text
+    /*班级类型*/
     .filter('classType', ['app',
         function (app) {
             return function (value) {
@@ -11,15 +32,15 @@ appFilters
         }
     ])
     //以下为仅需要value to text
+    /*记录可操作的按钮text*/
     .filter('operationText', ['app',
         function (app) {
             return function (op_name) {
-//        var status = _.findWhere(app.options.status, {value: input});
-//        return status?status['label']:'未知';
                 return app.Enum.OperationsText[op_name] || app.Enum.OperationsText[-1];
             };
         }
     ])
+    /*记录的状态 包括订单状态、课程和课程模板的状态*/
     .filter('statusText', ['app',
         function (app) {
             return function (value) {
@@ -29,6 +50,7 @@ appFilters
             };
         }
     ])
+    /*课程或者课程模板属于的类目*/
     .filter('categoryText', ['app',
         function (app) {
             return function (value) {
