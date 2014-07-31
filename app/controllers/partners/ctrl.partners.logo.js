ec53196f9f1437106f65f5ac58f5180a17354aa5
jzlingmo <120237546@qq.com>
2014-07-31 13:49:11 +0800

    create partner logo

--- app/controllers/partners/ctrl.partners.logo.js
+++ app/controllers/partners/ctrl.partners.logo.js
@@ -1,35 +1,35 @@
 'use strict';
 appControllers.controller('partnersLogoCtrl',
-    ['$scope','restAPI','$state', '$upload', 'app', function ($scope, restAPI, $state, $upload, app) {
-        var Partners = restAPI.partners;
-        var id = $state.params.id;
+    ['$scope', '$upload', 'app', function ($scope, $upload, app) {
+        var Partners = app.restAPI.partners;
+        var id = app.state.params.id;
         //TODO: replace with real url
         var uploadUrl = "../a-api/v2/" + "partner/" + id + "/logo";
-        $scope.onFileSelect = function($files) {
-            for ( var i = 0; i < $files.length; i++) {
+        $scope.onFileSelect = function ($files) {
+            for (var i = 0; i < $files.length; i++) {
                 var $file = $files[i];
                 $scope.logo = $file;
                 if ($scope.fileReaderSupported && $file.type.indexOf('image') > -1) {
                     var fileReader = new FileReader();
                     fileReader.readAsDataURL($files[i]);
-                    var loadFile = function(fileReader, index) {
-                        fileReader.onload = function(e) {
+                    var loadFile = function (fileReader, index) {
+                        fileReader.onload = function (e) {
                         }
                     }(fileReader, i);
                 }
             }
         };
-        $scope.upload = function() {
+        $scope.upload = function () {
             var files = [], fileNames = [];
             $scope.errorMsg = null;
 
             $upload.upload({
                 url: uploadUrl,
                 method: "POST",
-                data : {id: id},
+                data: {id: id},
                 file: $scope.logo,
                 fileFormDataName: "logo"
-            }).success(function(response) {
+            }).success(function (response) {
                 if (response.status > 0) {
                     app.toaster.pop("error", "logo上传失败", "");
                     $scope.errorMsg = response.status + ': ' + response.data;
@@ -37,7 +37,7 @@ appControllers.controller('partnersLogoCtrl',
                     app.toaster.pop("success", "logo上传成功", "");
                     app.state.go("admin.partners.detail", {id: id});
                 }
-            }).progress(function(evt) {
+            }).progress(function (evt) {
                 // Math.min is to fix IE which reports 200% sometimes
                 $scope.progress[index] = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
             });
