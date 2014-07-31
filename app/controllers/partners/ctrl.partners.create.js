1b0ed3d6cced4893b23000d6928f1a8dc61f5794
jzlingmo <120237546@qq.com>
2014-07-31 10:54:35 +0800

    partner create

--- app/controllers/partners/ctrl.partners.create.js
+++ app/controllers/partners/ctrl.partners.create.js
@@ -4,29 +4,26 @@ appControllers.controller('partnersCreateCtrl',
         var Partners = app.restAPI.partners;
         $scope.title = '这里是新建机构页面';
         $scope.partner = {};
-        $scope.partner.addressList = [""];
-        $scope.logo = {};
+        var addressList = $scope.partner.addressList = [""];
         $scope.addLocation = function () {
-            var len = $scope.partner.addressList.length;
-            $scope.partner.addressList[len] = "";
+            addressList.push("");
         };
         $scope.removeLocation = function (index) {
-            $scope.partner.addressList.splice(index,1);
+            addressList.splice(index,1);
 //            while (index < $scope.partner.addressList.length - 1) {
 //                if ($scope.partner.addressList[index] = $scope.partner.addressList[index + 1]);
 //                index++;
 //            }
 //            $scope.partner.addressList.pop();
         };
-        $scope.create = function(){
-            Partners.save($scope.partner, function(data){
-                //todo create success to do something
+        $scope.create = function(partner){
+            Partners.save(partner, function(data){
                 app.toaster.pop('success', "新建机构成功", "");
-                app.log.info('create partner success');
+                //todo 提示查看该条信息或者为该信息添加logo 照片或者添加教师
                 app.state.go('admin.partners.detail', {id: data.id});
 
             },function(){
-                app.log.error('create error');
+                app.toaster.pop('error', "新建机构失败", "");
             })
         };
     }]
