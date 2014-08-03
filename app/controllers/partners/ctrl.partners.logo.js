abb902ee6729f3e2a0b64b3306e9d6d0b06d23ce
jzlingmo <120237546@qq.com>
2014-08-03 12:38:40 +0800

    quick links

--- app/controllers/partners/ctrl.partners.logo.js
+++ app/controllers/partners/ctrl.partners.logo.js
@@ -2,9 +2,11 @@
 appControllers.controller('partnersLogoCtrl',
     ['$scope', '$upload', 'app', function ($scope, $upload, app) {
         var Partners = app.restAPI.partners;
-        var id = app.state.params.id;
+        var partnerId = $scope.partnerId = app.state.params.id;
+        //todo 初始化logo
+        $scope.logoUrl = undefined;
         //TODO: replace with real url
-        var uploadUrl = "../a-api/v2/" + "partner/" + id + "/logo";
+        var uploadUrl = "/a-api/v2/" + "partner/" + partnerId + "/logo";
         $scope.onFileSelect = function ($files) {
             for (var i = 0; i < $files.length; i++) {
                 var $file = $files[i];
@@ -26,24 +28,19 @@ appControllers.controller('partnersLogoCtrl',
             $upload.upload({
                 url: uploadUrl,
                 method: "POST",
-                data: {id: id},
-                file: $scope.logo,
-                fileFormDataName: "logo"
+                data: {partnerId: partnerId},
+                file: $scope.logo
             }).success(function (response) {
-                if (response.status > 0) {
-                    app.toaster.pop("error", "logo上传失败", "");
-                    $scope.errorMsg = response.status + ': ' + response.data;
-                } else {
-                    app.toaster.pop("success", "logo上传成功", "");
-                    app.state.go("admin.partners.detail", {id: id});
-                }
+                //todo 获取上传图片的url
+                app.toaster.pop("success", "logo上传成功", "");
+                app.state.go("admin.partners.detail", {id: id});
             }).progress(function (evt) {
                 // Math.min is to fix IE which reports 200% sometimes
                 $scope.progress[index] = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
             });
         };
         $scope.cancel = function () {
-            app.state.go('admin.partners.detail', {data: id});
+            app.state.go('admin.partners.detail', {id: partnerId});
         };
     }]
 );
