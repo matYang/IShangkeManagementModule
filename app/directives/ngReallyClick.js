fdb851dd9ab24394fc242f9a85f417f82ed4a77e
jzlingmo <120237546@qq.com>
2014-08-18 17:02:40 +0800

    pagination

---
+++ app/directives/ngReallyClick.js
@@ -0,0 +1,17 @@
+'use strict';
+/*
+ *
+ * */
+appDirectives.directive('ngReallyClick', [function() {
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs) {
+            element.bind('click', function() {
+                var message = attrs.ngReallyMessage;
+                if (message && confirm(message)) {
+                    scope.$apply(attrs.ngReallyClick);
+                }
+            });
+        }
+    }
+}]);
