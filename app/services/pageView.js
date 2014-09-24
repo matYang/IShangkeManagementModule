b2cdab6c51d2fc617268f0d0bbd441a322d5e973
Jet <120237546@qq.com>
2014-09-25 00:22:22 +0800

    add users manage view

--- app/services/pageView.js
+++ app/services/pageView.js
@@ -1,7 +1,8 @@
 'use strict';
 //table中的th
-app.factory('PageView', ['app','tools',
+app.factory('PageView', ['app', 'tools',
     function (app, tools) {
+        //这里主要配置结果页面table的header th
         var common = {
             statusTabs: [
                 {label: '已上线', active: true, value: {status: 2}},
@@ -9,7 +10,7 @@ app.factory('PageView', ['app','tools',
                 {label: '审核失败', active: false, value: {status: 1} },
                 {label: '已下线', active: false, value: {status: 3}}
             ],
-            bookingTh: [
+            bookingListTh: [
                 //n--name w-width percent
                 {n: '订单号', w: '15'},
                 {n: '入学人姓名', w: '10'},
@@ -18,6 +19,38 @@ app.factory('PageView', ['app','tools',
                 {n: '订单状态', w: '10'},
                 {n: '创建时间', w: '15'},
                 {n: '操作', w: '20'}
+            ],
+            courseListTh: [
+                {n: '课程号', w: '10'},
+                {n: '课程名', w: '20'},
+                {n: '爱上课价格', w: '12'},
+                {n: '原价', w: '12'},
+                {n: '类别', w: '12'},
+                {n: '状态', w: '12'},
+                {n: '操作', w: '22'}
+            ],
+            templateListTh:[
+                {n: '模板号', w: '10'},
+                {n: '模板名', w: '20'},
+                {n: '爱上课价格', w: '13'},
+                {n: '原价', w: '11'},
+                {n: '状态', w: '12'},
+                {n: '操作', w: '30'}
+            ],
+            partnerListTh: [
+                {n: '机构号', w: '10'},
+                {n: '机构名', w: '25'},
+                {n: '总部地址', w: '25'},
+                {n: '查询号', w: '25'},
+                {n: '操作', w: '15'}
+            ],
+            userListTh: [
+                {n: 'ID', w: '10'},
+                {n: '用户名', w: '15'},
+                {n: '真实姓名', w: '15'},
+                {n: '手机号', w: '20'},
+                {n: '邀请码', w: '15'},
+                {n: '注册时间', w: '25'}
             ]
         };
         return {
@@ -48,7 +81,30 @@ app.factory('PageView', ['app','tools',
                     order: 'desc',
                     createTimeStart: tools.getDeltaDayTimestamp()
                 },
-                th: common.bookingTh,
+                th: common.bookingListTh,
+                pagination: angular.copy(app.default_page),
+
+                search: {}//查询条件 存储用户手动输入的条件
+            },
+            users: {
+                tabs: [
+                    {
+                        label: '今日待处理', active: true,
+                        value: {statusSet: [0, 13], createTimeStart: tools.getDeltaDayTimestamp()}
+                    },
+                    {
+                        label: '往日待处理', active: false,
+                        value: {statusSet: [0, 13], createTimeEnd: tools.getDeltaDayTimestamp()}
+                    }
+                ],
+                //初始化过滤条件 tab的过滤条件 需要跟tabs处于active的tab的value相对应
+                filter: {
+                    statusSet: [0, 13],
+                    columnKey: 'createTime',
+                    order: 'desc',
+                    createTimeStart: tools.getDeltaDayTimestamp()
+                },
+                th: common.userListTh,
                 pagination: angular.copy(app.default_page),
 
                 search: {}//查询条件 存储用户手动输入的条件
@@ -58,39 +114,18 @@ app.factory('PageView', ['app','tools',
                 filter: {status: 2},//过滤条件 点击生成的条件
                 search: {},//查询条件 手动输入的条件
                 tabs: angular.copy(common.statusTabs),
-                th: [
-                    {n: '模板号', w: '10'},
-                    {n: '模板名', w: '20'},
-                    {n: '爱上课价格', w: '13'},
-                    {n: '原价', w: '11'},
-                    {n: '状态', w: '12'},
-                    {n: '操作', w: '30'}
-                ]
+                th: common.templateListTh
             },
             courses: {
                 pagination: angular.copy(app.default_page),
                 filter: {status: 2},//过滤条件 tab的过滤条件
                 search: {},//查询条件 手动输入的条件
                 tabs: angular.copy(common.statusTabs),
-                th: [
-                    {n: '课程号', w: '10'},
-                    {n: '课程名', w: '20'},
-                    {n: '爱上课价格', w: '12'},
-                    {n: '原价', w: '12'},
-                    {n: '类别', w: '12'},
-                    {n: '状态', w: '12'},
-                    {n: '操作', w: '22'}
-                ]
+                th: common.bookingListTh
             },
             partners: {
                 pagination: angular.copy(app.default_page),
-                th: [
-                    {n: '机构号', w: '10'},
-                    {n: '机构名', w: '25'},
-                    {n: '总部地址', w: '25'},
-                    {n: '查询号', w: '25'},
-                    {n: '操作', w: '15'}
-                ]
+                th: common.partnerListTh
             }
 
         }
